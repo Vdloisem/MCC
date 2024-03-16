4824e6cfcbeb70d46cdfb940e99ae83b07aa0c60
Vdloisem <mikel.vandeloise@student.unamur.be>
2024-03-16 20:44:14 +0100

    delete MCC_R_ANALYSIS/

--- LanguageType.hs
+++
@@ -1,9 +0,0 @@
-module LanguageType where
-
-import ParadigmType
-
--- Represents a programming language with its associated paradigms.
-data Language = Language {
-    languageName :: String,
-    languageParadigms :: [Paradigm]
-} deriving (Eq, Show)
